68618f5ca57169cfd687e133d416c3dd39be0202
Nick B <nbourrereg@hotmail.com>
2023-01-09 09:31:33 -0500

    Ajout du top down shooter

---
+++ godot/top_down_shooter/Enemy.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+var player
+
+var motion = Vector2()
+var hp = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_parent().get_node("Player")
+	
+	hp = rand_range(1, 5)
+	match hp:
+		1:
+			$Sprite.modulate = Color(.75, .75, .75)
+		2:
+			$Sprite.modulate = Color(1, 1, 0)
+		3:
+			$Sprite.modulate = Color(0, 1, 0)
+		4:
+			$Sprite.modulate = Color(0, 1, 1)
+		5:
+			$Sprite.modulate = Color(1, 0, 0)
+	
+
+func _physics_process(_delta):
+	position += (player.position - position)/50
+	look_at(player.position)
+
+	var _collision = move_and_collide(motion)
+
+	if hp <= 0:
+		queue_free()
+
+
+func _on_Area2D_body_entered(body:Node):
+	if "Bullet" in body.name:
+		hp -= 1
+		
---
+++ godot/top_down_shooter/Player.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+var move_speed = 500
+var motion = Vector2()
+
+var bullet_speed = 1000
+var bullet = preload("res://Bullet.tscn")
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(_delta):
+	
+
+	motion.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	motion.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+
+	motion = motion.normalized()
+	motion = move_and_slide(motion * move_speed)
+
+	look_at(get_global_mouse_position())
+
+	if (Input.is_action_just_pressed("lmb")):
+		fire()
+
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child", bullet_instance)
+
+func kill():
+	var _tmp = get_tree().reload_current_scene()
+	
+
+func _on_Area2D_body_entered(body:Node):
+	if "Enemy" in body.name:
+		kill()
+
