5e5508f6e021c71855210f19753c223ab16e1931
Nick B <nbourrerg@hotmail.com>
2023-03-23 14:55:13 -0400

    Clonario

---
+++ godot/Clonario/Food.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+var size
+
+func _ready():
+	randomize()
+	size = randf_range(0.5, 1.5)
+	$MeshInstance2D.scale = Vector2(size, size)
+	$CollisionShape2D.scale = Vector2(size, size)
+	$MeshInstance2D.modulate = Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255), 255)
+	
