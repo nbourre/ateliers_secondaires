c17c6555d2038d93f4b103065336f46a0ed4e32e
Nick B <nbourrereg@hotmail.com>
2023-10-27 14:54:28 -0400

    Modif du code

--- godot/top_down_polygon_shooter/enemy.gd
+++ godot/top_down_polygon_shooter/enemy.gd
@@ -4,10 +4,35 @@ var player
 var hp = 1
 var speed = 5
 
+@onready var shape = $Polygon2D
+static var count = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_parent().get_node("Player")
+	randomize()
+	#player = get_node("/root/world/Player")
+	player = get_parent().get_parent().get_node("Player")
 
+	count += 1
+	
+	if (count > 5):
+		hp = randi_range(1, 5)
+	
+	
+	match (hp):
+		1:
+			shape.color = Color(1, 1, 1)
+		2:
+			shape.color = Color(0, 0.8, 0)
+		3:
+			shape.color = Color(0, 0.8, 0)
+		4:
+			shape.color = Color(0, 0, 0.8)
+		5:
+			shape.color = Color(0.8, 0, 0)
+			scale.x = 1.5
+			scale.y = 1.5
+			
 func _physics_process(_delta):
 	var direction = (player.position - position)
 	direction = direction.normalized() * speed
@@ -16,13 +41,17 @@ func _physics_process(_delta):
 	
 	var _collision = move_and_collide(direction)	
 	
-	if (hp <= 0):
-		queue_free()
-
+	if (count <= 0):
+		var _tmp = get_tree().reload_current_scene()
 
 func _on_area_2d_area_entered(area):
 	if "Bullet" in area.get_parent().name:
 		hp -= 1;
-		area.get_parent().queue_free()
-		queue_free();
+		print(hp)
+		if (hp <= 0):
+			count -= 1
+			area.get_parent().queue_free()
+			queue_free();
+		
+
 	
