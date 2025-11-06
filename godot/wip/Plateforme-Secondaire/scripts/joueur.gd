0ced8023e256468b46a54f15320f10bd0eebfcf4
Nick B <nbourrerg@hotmail.com>
2025-11-06 11:18:18 -0500

    structure et ajout slime

--- godot/wip/Plateforme-Secondaire/scripts/joueur.gd
+++ godot/wip/Plateforme-Secondaire/scripts/joueur.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+@export var SPEED : float = 130.0
+@export var JUMP_VELOCITY : float = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
