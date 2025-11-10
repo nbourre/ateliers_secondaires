45363a85f87864acf7fbfd890386b1816aed9f96
Nick B <nbourrereg@hotmail.com>
2025-11-10 13:33:22 -0500

    rc1

--- godot/wip/Plateforme-Secondaire/scripts/pièce.gd
+++ godot/wip/Plateforme-Secondaire/scripts/pièce.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
-@onready var gestionnaire_jeu : Node = %GestionnaireJeu
+var gestionnaire_jeu : Node
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	if get_tree().get_root().has_node("Monde/GestionnaireJeu"):
+		gestionnaire_jeu = get_tree().get_root().get_node("Monde/GestionnaireJeu")
+
+
 func _on_body_entered(_body: Node2D) -> void:
-	gestionnaire_jeu.ajouter_point()
+	if gestionnaire_jeu != null:
+		gestionnaire_jeu.ajouter_point()
 	animation_player.play("ramassage")
