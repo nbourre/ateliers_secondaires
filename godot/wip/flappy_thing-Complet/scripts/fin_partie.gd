2cd4d25038a1016fda41998229f6e6803a36b3a5
Nick B <nbourrereg@hotmail.com>
2026-01-08 08:24:22 -0500

    removed project bad branch

--- godot/wip/flappy_thing-Complet/scripts/fin_partie.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-signal redemarrer
-
-func _on_redemarrer_btn_pressed() -> void:
-	redemarrer.emit()
--- godot/wip/flappy_thing-Complet/scripts/sol.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-signal sol_touche
-
-func _on_body_entered(body: Node2D) -> void:
-	sol_touche.emit()
