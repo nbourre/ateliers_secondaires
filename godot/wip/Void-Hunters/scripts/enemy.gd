8ecbde55df0a863fd0be8e14d153f482a652f618
Nick B <nbourrereg@hotmail.com>
2026-02-16 10:29:15 -0500

    Ajout du débogage pour la distance

--- godot/wip/Void-Hunters/scripts/enemy.gd
+++ godot/wip/Void-Hunters/scripts/enemy.gd
@@ -6,6 +6,10 @@ const JUMP_VELOCITY = -400.0
 const MAX_STEER_SPEED = 200.0
 
 var target : Node2D
+var distance := Vector2.ZERO
+
+var print_acc_time := 0.0
+var print_last_time := 0.0
 
 func _ready() -> void:
 	pass
@@ -13,11 +17,18 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if target != null:
 		
-		var direction := (target.position - position).normalized()
+		distance = target.position - position
+
+		# Une distance de 500 semble bien pour débuter à tirer
+
+
+		var direction := distance.normalized()
 
 		look_at(target.position)
 		velocity = direction * SPEED
 		move_and_slide()
+
+		task_print_distance(delta)
 
 
 func set_target(new_target: Node2D) -> void:
@@ -25,3 +36,13 @@ func set_target(new_target: Node2D) -> void:
 
 func steer_at (position : Vector2) -> void:
 	pass
+
+func task_print_distance (delta : float) -> void:
+	const rate := 1.0
+
+	print_acc_time += delta
+
+	if print_acc_time > rate:
+		print_acc_time = 0.0
+		print("Distance to target : ", distance.length())
+		
